42534bd5e02ce7dabb2d1f693fe52c7bbbf48623
parkeunae <eapark108@gmail.com>
2019-09-25 07:40:19 +0900

    add crud design

--- parkeunae1902/todo/src/main/webapp/src/Contents.js
+++ parkeunae1902/todo/src/main/webapp/src/Contents.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import Checkbox from './Checkbox';
 import './Contents.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit } from '@fortawesome/free-regular-svg-icons';
+import { faEdit, faTimesCircle, faCheckCircle } from '@fortawesome/free-regular-svg-icons';
+import { OverlayTrigger, Tooltip, Dropdown } from 'react-bootstrap';
 
 const types = [
 	"Todo",
@@ -43,11 +44,11 @@ class List extends Component {
 	render() {
 		const listType = this.props.title;
 		return (
-			<section className="Contents-list">
+			<section className="Contents-list" id={listType+'-list-id'}>
 				<Subject title={this.props.title}></Subject>
 				{datas.filter(data => data.type === listType)
 					.map((data, index) => {
-						return <Cards contents={data.contents} id={data.type+index} key={index}></Cards>
+						return <Cards contents={data.contents} id={data.type+index} type={data.type} key={index}></Cards>
 					})}
 			</section>
 		)
@@ -61,6 +62,27 @@ class Subject extends Component {
 				<h3 className="Contents-title">
 					{this.props.title}
 				</h3>
+				<label htmlFor={this.props.title+'Select'}>
+					
+				</label>
+				<Dropdown id={this.props.title+'Select'}>
+					<Dropdown.Toggle variant="warning" id="dropdown-variants-warning">
+					</Dropdown.Toggle>
+					<Dropdown.Menu>
+						{
+							!(this.props.title === 'Todo') && 
+							<Dropdown.Item href="">Todo</Dropdown.Item>
+						}
+						{
+							!(this.props.title === 'Doing') && 
+							<Dropdown.Item href="">Doing</Dropdown.Item>
+						}
+						{
+							!(this.props.title === 'Done') && 
+							<Dropdown.Item href="">Done</Dropdown.Item>
+						}	
+					</Dropdown.Menu>
+				</Dropdown>
 			</div>
 		)
 	}
@@ -68,18 +90,102 @@ class Subject extends Component {
 
 
 class Cards extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isComplete: true,
+		};
+		this.changeCompleteState = this.changeCompleteState.bind(this);
+	}
+
+	changeCompleteState() {
+		this.setState({isComplete: true});
+	}
+
+	modifyCardContents = () => {
+		this.setState({isComplete: false});
+	}
+
 	render() {
 		return (
-			<div className="Contents-Cards">
-				<Checkbox id={this.props.id} />
-				<div className="Cards-contents">
-					<p>{this.props.contents}</p>
+			<div className="Contents-Cards" id={this.props.id+'card'}>
+				{
+					this.state.isComplete === true ?
+					<div>
+					<Checkbox id={this.props.id} />
+					<div className="Cards-contents">
+						<p>{this.props.contents}</p>
+					</div></div> 
+					: 
+					<CardEdititor id={this.props.id} value={this.props.contents} changeCompleteState={this.changeCompleteState}></CardEdititor>
+				}
+				{
+					this.props.type === 'Todo' && this.state.isComplete === true ?
+					<div className="Cards-modify-btn">
+						<OverlayTrigger placement="bottom" 
+														overlay={
+															<Tooltip >수정하기</Tooltip>
+														}>
+							<button onClick={this.modifyCardContents}>
+								<FontAwesomeIcon icon={faEdit} size="1x" ></FontAwesomeIcon>
+							</button>
+						</OverlayTrigger>
+					</div> : ''
+				}
+				
+			</div>
+		)
+	}
+}
+
+class CardEdititor extends Component {
+	
+	resizeCardEditor() {
+		const textArea = document.querySelector(".Cards-textarea");
+		const textHeight = textArea.style.height;
+
+		if(textArea.scrollHeight > textHeight) {
+			textArea.style.height = textArea.scrollHeight + 'px';
+		}
+	}
+
+	cancelRegisterCard = (e) => {
+		const cardCreated = document.querySelector(".Card-created");
+		cardCreated.parentElement.lastElementChild.remove();
+
+		const CreateBtn = document.querySelector("#create-card");
+		if(CreateBtn.getAttribute("disabled") === "disabled") {
+			CreateBtn.removeAttribute("disabled");
+		}
+    
+	}
+
+	render() {
+		return (
+			<div className="Cards-contents">
+				<div>
+					<OverlayTrigger placement="left" 
+													overlay={
+														<Tooltip >취소</Tooltip>
+													}>
+						<button className="Cards-cancel-btn" onClick={this.props.value === "" ? this.cancelRegisterCard : this.props.changeCompleteState}>
+							<FontAwesomeIcon icon={faTimesCircle} size="1x" ></FontAwesomeIcon>
+						</button>
+					</OverlayTrigger>
 				</div>
-				<div className="Cards-modify-btn">
-					<a href="">
-						<FontAwesomeIcon icon={faEdit} size="1x" ></FontAwesomeIcon>
-					</a>
+
+				<div>
+					<OverlayTrigger placement="left" 
+													overlay={
+														<Tooltip >저장</Tooltip>
+													}>
+						<button className="Cards-save-btn">
+							<FontAwesomeIcon icon={faCheckCircle} size="1x" ></FontAwesomeIcon>
+						</button>
+					</OverlayTrigger>
 				</div>
+				
+				<textarea className="Cards-textarea" placeholder="일정을 입력해주세요!" onKeyDown={this.resizeCardEditor} defaultValue={this.props.value}></textarea>
 			</div>
 		)
 	}
@@ -87,3 +193,4 @@ class Cards extends Component {
 
 
 export default Contents;
+export {CardEdititor};
--- parkeunae1902/todo/src/main/webapp/src/Mainpage.js
+++ parkeunae1902/todo/src/main/webapp/src/Mainpage.js
@@ -1,20 +1,33 @@
 import React, {Component} from 'react';
-import Contents from './Contents';
+import ReactDOM from 'react-dom';
+import Contents, {CardEdititor} from './Contents';
 import './Mainpage.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckSquare } from '@fortawesome/free-regular-svg-icons';
 import { Button } from 'react-bootstrap';
 
 class Header extends Component {
+  createCard = () => {
+    const todolist = document.querySelector('#Todo-list-id');
+    const elementCreated = document.createElement('div');
+    elementCreated.className = "Contents-Cards Card-created";
+    todolist.appendChild(elementCreated);
+    
+    ReactDOM.render(<CardEdititor></CardEdititor>, elementCreated);
+
+    const CreateBtn = document.querySelector("#create-card");
+    CreateBtn.setAttribute("disabled", "disabled");
+  }
+
 	render() {
 		return (
 			<header>
-                <FontAwesomeIcon icon={faCheckSquare} size="3x" ></FontAwesomeIcon>
-                <h1 className="Mainpage-header">{this.props.title}</h1>
-                <div className="Mainpage-header-btn">
-                    <Button variant="dark">일정 추가</Button>
-                    <Button variant="secondary">일정 삭제</Button>
-                </div>
+        <FontAwesomeIcon icon={faCheckSquare} size="3x" ></FontAwesomeIcon>
+        <h1 className="Mainpage-header">{this.props.title}</h1>
+        <div className="Mainpage-header-btn">
+          <Button id="create-card" variant="warning" onClick={this.createCard}>일정 추가</Button>
+          <Button id="delete-card" variant="light">일정 삭제</Button>
+        </div>
 			</header>
 		)
 	}
